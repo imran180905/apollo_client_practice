f14498e44ae5e776724c59eb8b7f722bfdb8c806
Imran <aliimran718779@gmail.com>
2023-06-13 11:25:30 +0600

    Template

---
+++ src/queries/queries.ts
@@ -0,0 +1,19 @@
+const GET_CHARACTERS_QUERY = gql`
+  query GetCharacters($page: Int!) {
+    characters(page: $page) {
+      info {
+        count
+        pages
+        next
+        prev
+      }
+      results {
+        id
+        name
+        status
+        species
+        type
+      }
+    }
+  }
+`;
