3f148829fb1ef7fb4bd8e608f691e0e5b6d1ae4c
MansuraAkhter <mansura3330@gmail.com>
2023-06-16 16:31:01 +0600

    loading status updated

--- src/components/NewsAsset/newsAssetList.tsx
+++ src/components/NewsAsset/newsAssetList.tsx
@@ -1,33 +1,41 @@
 import client from "@/graphqlClents/client";
 import { getNewsListQuery } from "@/queries/newAssetQueries";
 import { useQuery, useLazyQuery } from "@apollo/client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import _ from 'lodash';
 
 
 
 const NewsAssetList = () => {
     const [name, setName] = useState('');
+    const [loadfirst, setLoadfirst] = useState(false);
+
+    useEffect(() => {
+        if (loading) setLoadfirst(true);
+    }, [])
 
     const { loading, error, data, fetchMore } = useQuery(getNewsListQuery, {
         variables: {
             searchWord: name,
             pageNumber: 1,
-            perPage: 10
-        }
+            perPage: 10,
+        },
+        notifyOnNetworkStatusChange: true,
     })
     console.log(data?.getRegisteredNewsAssetList?.newsAssetList);
 
     const loadMoreCharacters = () => {
+        setLoadfirst(false);
+
         fetchMore({
             variables: {
                 searchWord: name,
                 pageNumber: data?.getRegisteredNewsAssetList?.pagination?.currentPage + 1,
-                perPage: 10
+                perPage: 10,
             },
+
             updateQuery: (prevResult, { fetchMoreResult }) => {
                 if (!fetchMoreResult) return prevResult;
-
                 return {
                     getRegisteredNewsAssetList: {
                         ...fetchMoreResult.getRegisteredNewsAssetList,
@@ -38,14 +46,18 @@ const NewsAssetList = () => {
                     },
                 };
             },
+
         });
+
+
+
     };
 
     const handleChange = (e: any) => {
         setName(e.target.value)
     };
 
-    const debouncedOnChange = _.debounce(handleChange, 600)
+    const debouncedOnChange = _.debounce(handleChange, 1000)
     return (<div>
         news asset list
 
@@ -61,9 +73,14 @@ const NewsAssetList = () => {
                 )
             })
         }
-        <button onClick={loadMoreCharacters}>load more...</button>
+        {loading && !loadfirst && <p>loading..</p>}
+        {loading && loadfirst && <p>FirstLoading...</p>}
+        {!loading && data?.getRegisteredNewsAssetList?.pagination?.currentPage !== data?.getRegisteredNewsAssetList?.pagination?.totalPages && <button onClick={loadMoreCharacters}>load more</button>}
+        {!loading && data?.getRegisteredNewsAssetList?.pagination?.currentPage === data?.getRegisteredNewsAssetList?.pagination?.totalPages && <p>End of list</p>}
+
 
     </div>);
 }
 
 export default NewsAssetList;
+
