3248bcc0b01e00e6d9656c7e89a7bbcae378f447
MansuraAkhter <mansura3330@gmail.com>
2023-06-16 10:25:26 +0600

    search and fetch more implimented

--- src/components/NewsAsset/newsAssetList.tsx
+++ src/components/NewsAsset/newsAssetList.tsx
@@ -1,24 +1,57 @@
 import client from "@/graphqlClents/client";
 import { getNewsListQuery } from "@/queries/newAssetQueries";
-import { useQuery } from "@apollo/client";
+import { useQuery, useLazyQuery } from "@apollo/client";
 import { useState } from "react";
+import _ from 'lodash';
 
 
 
 const NewsAssetList = () => {
+    const [name, setName] = useState('');
 
-    const { loading, error, data } = useQuery(getNewsListQuery, {
+    const { loading, error, data, fetchMore } = useQuery(getNewsListQuery, {
         variables: {
-            searchWord: "",
+            searchWord: name,
             pageNumber: 1,
             perPage: 10
         }
     })
-    console.log(data?.getRegisteredNewsAssetList?.newsAssetList)
+    console.log(data?.getRegisteredNewsAssetList?.newsAssetList);
+
+    const loadMoreCharacters = () => {
+        fetchMore({
+            variables: {
+                searchWord: name,
+                pageNumber: data?.getRegisteredNewsAssetList?.pagination?.currentPage + 1,
+                perPage: 10
+            },
+            updateQuery: (prevResult, { fetchMoreResult }) => {
+                if (!fetchMoreResult) return prevResult;
+
+                return {
+                    getRegisteredNewsAssetList: {
+                        ...fetchMoreResult.getRegisteredNewsAssetList,
+                        newsAssetList: [
+                            ...prevResult.getRegisteredNewsAssetList.newsAssetList,
+                            ...fetchMoreResult.getRegisteredNewsAssetList.newsAssetList,
+                        ],
+                    },
+                };
+            },
+        });
+    };
+
+    const handleChange = (e: any) => {
+        setName(e.target.value)
+    };
+
+    const debouncedOnChange = _.debounce(handleChange, 600)
     return (<div>
         news asset list
+
+        <input type="text" onChange={debouncedOnChange} placeholder='search' />
         {
-            data?.getRegisteredNewsAssetList?.newsAssetList.map((news, index: number) => {
+            data?.getRegisteredNewsAssetList?.newsAssetList.map((news: any, index: number) => {
                 return (
                     <div key={index}>
                         <h1>{news.asseetName}</h1>
@@ -28,6 +61,7 @@ const NewsAssetList = () => {
                 )
             })
         }
+        <button onClick={loadMoreCharacters}>load more...</button>
 
     </div>);
 }
