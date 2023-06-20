381a04e3973f80da100ee46656ac3dba9f7c942a
MansuraAkhter <mansura3330@gmail.com>
2023-06-20 15:25:27 +0600

    usefecth hook added  to fetch data

--- src/components/NewsAsset/newsAssetList.tsx
+++ src/components/NewsAsset/newsAssetList.tsx
@@ -1,64 +1,16 @@
-import client from "@/graphqlClents/client";
+import useFetch from "../hooks/usefetch";
 import { getNewsListQuery } from "@/queries/newAssetQueries";
-import { useQuery, useLazyQuery } from "@apollo/client";
-import { useState, useEffect, useRef } from "react";
+import { useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
 import _ from 'lodash';
 
 
 
 const NewsAssetList = () => {
-    const [name, setName] = useState('');
     const [loadfirst, setLoadfirst] = useState(false);
-
     const perPage = 10;
+    const [debouncedOnChange, loadMoreCharacters, data, loading] = useFetch(perPage, setLoadfirst);
 
-    useEffect(() => {
-        if (loading) setLoadfirst(true);
-    }, [])
-
-    const { loading, error, data, fetchMore } = useQuery(getNewsListQuery, {
-        variables: {
-            searchWord: name,
-            pageNumber: 1,
-            perPage: perPage,
-        },
-        notifyOnNetworkStatusChange: true, // true: adds loading status true for initial load and load more, false: adds loading status true only on initial load
-
-    })
-    console.log(data?.getRegisteredNewsAssetList?.newsAssetList);
-
-    const loadMoreCharacters = () => {
-        setLoadfirst(false);
-
-        fetchMore({
-            variables: {
-                searchWord: name,
-                pageNumber: data?.getRegisteredNewsAssetList?.pagination?.currentPage + 1,
-                perPage: perPage,
-            },
-
-            updateQuery: (prevResult, { fetchMoreResult }) => {
-                if (!fetchMoreResult) return prevResult;
-                return {
-                    getRegisteredNewsAssetList: {
-                        ...fetchMoreResult.getRegisteredNewsAssetList,
-                        newsAssetList: [
-                            ...prevResult.getRegisteredNewsAssetList.newsAssetList,
-                            ...fetchMoreResult.getRegisteredNewsAssetList.newsAssetList,
-                        ],
-                    },
-                };
-            },
-
-        });
-
-    };
-
-    const handleChange = (e: any) => {
-        setName(e.target.value)
-    };
-
-    const debouncedOnChange = _.debounce(handleChange, 1000)
     return (<div>
         news asset list
 
---
+++ src/components/hooks/usefetch.ts
@@ -0,0 +1,61 @@
+import { getNewsListQuery } from "@/queries/newAssetQueries";
+import { useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
+import _ from 'lodash';
+
+
+
+export default function useFetch (perPage:number,setLoadfirst:any) {
+    const [name, setName] = useState('');
+   
+
+    useEffect(() => {
+        if (loading) setLoadfirst(true);
+    }, [])
+
+    const { loading, error, data, fetchMore } = useQuery(getNewsListQuery, {
+        variables: {
+            searchWord: name,
+            pageNumber: 1,
+            perPage: perPage,
+        },
+        notifyOnNetworkStatusChange: true, // true: adds loading status true for initial load and load more, false: adds loading status true only on initial load
+
+    })
+    console.log(data?.getRegisteredNewsAssetList?.newsAssetList);
+
+    const loadMoreCharacters = () => {
+        setLoadfirst(false);
+
+        fetchMore({
+            variables: {
+                searchWord: name,
+                pageNumber: data?.getRegisteredNewsAssetList?.pagination?.currentPage + 1,
+                perPage: perPage,
+            },
+
+            updateQuery: (prevResult, { fetchMoreResult }) => {
+                if (!fetchMoreResult) return prevResult;
+                return {
+                    getRegisteredNewsAssetList: {
+                        ...fetchMoreResult.getRegisteredNewsAssetList,
+                        newsAssetList: [
+                            ...prevResult.getRegisteredNewsAssetList.newsAssetList,
+                            ...fetchMoreResult.getRegisteredNewsAssetList.newsAssetList,
+                        ],
+                    },
+                };
+            },
+
+        });
+
+    };
+
+    const handleChange = (e: any) => {
+        setName(e.target.value)
+    };
+
+    const debouncedOnChange = _.debounce(handleChange, 1000)
+    return [debouncedOnChange,loadMoreCharacters,data,loading]
+}
+ 
